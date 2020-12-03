646a8ce91ab87c81b38f061d2c4976d979812d97
athar <athar.adel@gmx.de>
2020-12-03 18:24:49 +0100

    Start for Calculator

---
+++ src/tr_test/GUI.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package tr_test;
+
+/**
+ * @author User
+ *
+ */
+public class GUI {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
